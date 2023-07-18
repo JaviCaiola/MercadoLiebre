101a01c2b84cb71aa479adb41b295b630fc7a3ba
JaviCaiola <javiteen1250@gmail.com>
2023-07-18 11:30:39 -0300

    borrando los paquetes de node

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(4432, ()=> console.log("server ok"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+})
