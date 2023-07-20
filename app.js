0f31ad624509e4f35955df920ebaebf2d09f9001
JaviCaiola <javiteen1250@gmail.com>
2023-07-20 00:48:44 -0300

    solucionando problema de lectura de archivos en el server expres

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.listen(4432, ()=> console.log("server ok"));
 
+app.use(express.static(__dirname+'/'));
+app.use('/img', express.static(__dirname+'public/img'));
+app.use('/css', express.static(__dirname+'public/css'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
