b0be898566162fc166d55c45ef944b93face1855
JaviCaiola <javiteen1250@gmail.com>
2023-07-20 21:47:21 -0300

    modificando app.js para que lea los scripts

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.listen(4432, ()=> console.log("server ok"));
 app.use(express.static(__dirname+'/'));
 app.use('/img', express.static(__dirname+'public/img'));
 app.use('/css', express.static(__dirname+'public/css'));
+app.use('/js', express.static(__dirname+'public/js'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
