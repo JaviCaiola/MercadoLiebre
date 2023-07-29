886708b176554197658f929ce9641b9c0e131786
JaviCaiola <javiteen1250@gmail.com>
2023-07-28 23:43:42 -0300

    configurando el app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.static(__dirname+'/'));
 app.use('/img', express.static(__dirname+'public/img'));
 app.use('/css', express.static(__dirname+'public/css'));
 app.use('/js', express.static(__dirname+'public/js'));
+app.use('/svg', express.static(__dirname+'public/svg'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
