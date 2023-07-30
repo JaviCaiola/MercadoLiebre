5f78482e6be4bb0466ed47bf904b38e97e1b08e5
JaviCaiola <javiteen1250@gmail.com>
2023-07-30 05:12:42 -0300

    funcion de zoom implementada

--- public/js/index.js
+++ public/js/index.js
@@ -9,3 +9,27 @@ boton.addEventListener('click', () => {
 botoncerrar.addEventListener('click', ()=>{
     slidemenu.classList.toggle('activo')
 } )
+
+const zoomImages = document.querySelectorAll('.zoomeable');
+
+zoomImages.forEach((img) => {
+  const container = img.parentElement;
+  const zoomEffect = document.createElement('div');
+  zoomEffect.classList.add('zoom-effect');
+  container.appendChild(zoomEffect);
+
+  container.addEventListener('mousemove', (e) => {
+    const { offsetX, offsetY } = e;
+    const { offsetWidth, offsetHeight } = container;
+    const x = (offsetX / offsetWidth) * 100;
+    const y = (offsetY / offsetHeight) * 100;
+
+    zoomEffect.style.backgroundImage = `url(${img.src})`;
+    zoomEffect.style.backgroundPosition = `${x}% ${y}%`;
+    zoomEffect.style.opacity = '1';
+  });
+
+  container.addEventListener('mouseleave', () => {
+    zoomEffect.style.opacity = '0';
+  });
+});
