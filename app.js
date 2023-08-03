5c44265dc5808252582e54dba058190f2fd74150
JaviCaiola <javiteen1250@gmail.com>
2023-08-03 10:12:31 -0300

    creando archivo de registro y conexion al path

--- app.js
+++ app.js
@@ -13,3 +13,6 @@ app.use('/svg', express.static(__dirname+'public/svg'));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/registro.html'));
+})
