d73b0a9f234b3cf902988e3cf5797a0df08523b9
JaviCaiola <javiteen1250@gmail.com>
2023-08-10 09:45:29 -0300

    update queryes 2.0

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,11 @@
-let openmodal = document.querySelectorAll('.openmod');
+let openmodal = document.querySelector('.openmod');
+let openmod = document.querySelector('.openmodal');
 let modal = document.querySelector('.modal-session');
 let cerrarmodal = document.querySelector('.close-modal');
 
+openmod.addEventListener('click', ()=>{
+    modal.classList.toggle('activemod');
+})
 openmodal.addEventListener('click', () => {
     modal.classList.toggle('activemod');
 })
